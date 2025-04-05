0a83b3b7134037e4d173a9f91be1a311d6a1bca5
Andrey S <andrey.n.solyanik@gmail.com>
2025-04-05 18:52:05 +0500

    Project structure

---
+++ frontend/scripts/app.js
@@ -0,0 +1,11 @@
+import { Router } from "./router.js";
+
+class App {
+    constructor() {
+        this.router = new Router();
+        window.addEventListener('DOMContentLoaded', this.router.openRoute())
+        window.addEventListener('popstate', this.router.openRoute())
+    }
+}
+
+(new App());
